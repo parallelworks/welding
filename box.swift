e9d879e6acb98d2e9b18eb4ca991b3073f2cde52
mmehrabadi3 <marmar@gatech.edu>
2017-05-10 13:18:43 -0400

    box.swift now passes arg's from command line to Salome Python script

--- box.swift
+++ box.swift
@@ -4,19 +4,34 @@ type file;
 
 # ------ INPUT / OUTPUT DEFINITIONS -------#
 
-file fgeom <"BoxMesh/box.step">;
-file meshScript <"BoxMesh/boxMesh_noArgs.py">;
-file fmesh <"box_mesh.unv">;
+string geomFileName = arg("geomFileName", "box.step");
+string meshFileName = arg("meshFileName", "box_mesh.unv");
+
+file fgeom <strcat("inputs/",geomFileName)>;
+file fmesh <strcat("outputs/",meshFileName)>;
+file meshScript <"utils/boxMesh_args.py">;
+
+float Length  = parseFloat(arg("Length", "10.0"));
+float Width  = parseFloat(arg("Width", "10.0"));
+float Height  = parseFloat(arg("Height", "1.0"));
+float meshScale  = parseFloat(arg("meshScale", "1.0"));
+float highResWidth = parseFloat(arg("highResWidth","3.0"));
 
 
 # -- APP DEFINITIONS
 
-#app (file fmesh, file feff, file salemeLog) meshBox (file fgeom, file salomeScript) {
-app (file fmesh) meshBox (file fgeom, file meshScript) {
-	salome "start" "-t" "-w 1" filename(meshScript);
+app (file fmesh) meshBox (file meshScript, float Length, float Width, float Height, float highResWidth, float meshScale, file fgeom) {
+    # Example of running Python mesh script by Salome :
+    # salome shell   boxMesh_args.py args:10,10,1,inputs/box.step,3,1,outputs/box_mesh.unv
+	salome "start" "-t" "-w 1" filename(meshScript)  strcat("args:", Length, ",",
+                                                                     Width, ",", 
+                                                                     Height, ",", 
+                                                                     filename(fgeom), ",",
+                                                                     highResWidth, ",",
+                                                                     meshScale, ",",
+                                                                     filename(fmesh));
 }
 
 #----------------workflow---------#
 
-fmesh = meshBox(fgeom, meshScript);
-
+fmesh = meshBox(meshScript, Length, Width, Height, highResWidth, meshScale, fgeom);
