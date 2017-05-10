adc9e02540384b00286d9cc45d96c4eda319e704
mmehrabadi3 <marmar@gatech.edu>
2017-05-10 17:13:15 -0400

    In box.swift, passing a file mapper instead of python module files.

--- box.swift
+++ box.swift
@@ -9,25 +9,27 @@ string meshFileName = arg("meshFile", "box_mesh.unv");
 string geomMeshParamsFileName = arg("geomMeshParamsFile", "geomMeshParams.in");
 
 file fgeom <strcat("inputs/",geomFileName)>;
-file fgeomMeshParams  <strcat("inputs/",geomMeshParamsFileName)>;
+file fsimParams  <strcat("inputs/",geomMeshParamsFileName)>;
 
-file meshScript <"utils/boxMesh_inputFile_noArgs.py">;
-file pythonLib <"utils/data_IO.py">;  # Python functions for reading data from files
+file meshScript <"utils/boxMesh_inputFile.py">;
+file utils[] 		<filesys_mapper;location="utils">;
 
 file fmesh <strcat("outputs/",meshFileName)>;
 file salomeErr <"outputs/salome.err">;                        
 
 # -- APP DEFINITIONS
 
-app (file fmesh, file ferr) meshBox (file meshScript, file fgeom, file pythonLib, file fgeomMeshParams ) {
-    # Example of running Python mesh script by Salome :
-    # salome start -t -w 1 boxMesh_inputFile.py args:inputs/geomMeshParams.in,inputs/box.step,outputs/box_mesh.unv 
+#app (file fsimParams) writeGeomMeshParams(file paramScript, 
+
+# Generate mesh by running a Salome Python script
+# Example of running Python mesh script by Salome :
+# salome start -t -w 1 boxMesh_inputFile.py args:inputs/geomMeshParams.in,inputs/box.step,outputs/box_mesh.unv 
+app (file fmesh, file ferr) makeMesh (file meshScript, file fgeom, file utils[], file fsimParams ) {
     salome "start" "-t" "-w 1" filename(meshScript) stderr=filename(ferr) 
-                               strcat("args:",filename(fgeomMeshParams), ",", filename(fgeom), ",", filename(fmesh));
+                               strcat("args:",filename(fsimParams), ",", 
+                                              filename(fgeom), ",", filename(fmesh));
 }
 
 #----------------workflow---------#
 
-
-
-(fmesh, salomeErr) = meshBox(meshScript, fgeom, pythonLib, fgeomMeshParams );
+(fmesh, salomeErr) = makeMesh(meshScript, fgeom, utils, fsimParams);
