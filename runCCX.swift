253fbb50396da2ae1e107a56a887ce68caed46bf
mmehrabadi3 <marmar@gatech.edu>
2017-05-12 10:57:29 -0400

    Added the genAnimation.sh to the Swift workflow.

--- runCCX.swift
+++ runCCX.swift
@@ -6,14 +6,15 @@ type file;
 string meshFileName = arg("meshFile", "box");
 
 string ccxFolder = "utils/ccx-212";
-file ccxBin                <strcat(ccxFolder,"/src/","ccx_2.12")>;
+file ccxBin                 <strcat(ccxFolder,"/src/","ccx_2.12")>;
 file ccxSrc                 <strcat(ccxFolder,".tar.gz")>; 
 file ccxComplieScript       <"utils/compileCcx.sh">;
 file getCcxInpScript        <"utils/writeCCXinpFile.py">;
 file fgenericInp            <"inputs/solve.inp">;
 file fluxRoutine            <"utils/dflux.f">;
 file ccxExecScript          <"utils/runCcxBinary.sh">;
-file utils[] 		      <filesys_mapper;location="utils", suffix=".py">;
+file utils[] 		        <filesys_mapper;location="utils", suffix=".py">;
+file makeAnimScript         <"utils/genAnimationInDir.sh">;
 
 # ------ APP DEFINITIONS --------------------#
 
@@ -29,16 +30,20 @@ app (file fsol, file fsta, file fcvg, file fdat, file fOut, file ferr) runCcx (f
 	bash filename(ccxExecScript) filename(ccxBin)  caseName stderr=filename(ferr) stdout=filename(fOut);
 }
 
+app (file fanim, file[] fpngs, file fOut, file ferr) makeAnimation (file makeAnimScript, file fsol, string caseDir){
+	bash filename(makeAnimScript) caseDir stderr=filename(ferr) stdout=filename(fOut);
+}
+
 #----------------workflow-------------------#
 
 int i = 1;
-string outCaseDir = "outputs/case";
+string outCaseDir = strcat("outputs/case", i, "/");
 
-file fccxErr       <strcat(outCaseDir, i, "/ccx.err")>;
-file fccxOut       <strcat(outCaseDir, i, "/ccx.out")>;
-string caseName = strcat(outCaseDir, i,"/solve");
+file fccxErr       <strcat(outCaseDir, "ccx.err")>;
+file fccxOut       <strcat(outCaseDir, "ccx.out")>;
+string caseName = strcat(outCaseDir,"solve");
 
-file fmsh4ccx     <strcat(outCaseDir, i, "/", meshFileName, ".msh")>;
+file fmsh4ccx     <strcat(outCaseDir, meshFileName, ".msh")>;
 
 file finp         <strcat(caseName,".inp")>;
 file fsol         <strcat(caseName,".frd")>;
@@ -51,3 +56,11 @@ ccxBin = compileCcx(ccxComplieScript, ccxFolder, ccxSrc, fluxRoutine);
 finp = getCcxInp(getCcxInpScript, fgenericInp, fmsh4ccx, utils);
 
 (fsol, fsta, fcvg, fdat, fccxOut, fccxErr) = runCcx(ccxExecScript, ccxBin, fmsh4ccx, caseName, finp);
+
+
+file fanim       <strcat(outCaseDir,"temp.gif")>;
+file fanimOut       <strcat(outCaseDir,"anim.out")>;
+file fanimErr       <strcat(outCaseDir,"anim.err")>;
+file[] fpngs     <filesys_mapper;location=strcat(outCaseDir,"pngs")>;
+
+(fanim, fpngs, fanimOut, fanimErr) = makeAnimation(makeAnimScript, fsol, outCaseDir);
---
+++ utils/genAnimation.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+dir=/core/
+
+rm pngs -R > /dev/null 2>&1
+mkdir -p pngs
+
+timestep=0.1
+simtime=0.5
+
+timesteps=$(echo $simtime/$timestep | bc)
+#timesteps=1
+
+startDS=2 # specific to NDTEMP
+numElements=4
+
+maxDatasets=$(echo $numElements*$timesteps | bc)
+
+element=1
+
+frame=1
+for ds in $(seq 2 $numElements $maxDatasets);do
+
+echo $frame,$ds,$element
+
+cat > tmp.fbd <<END
+    read solve.frd new
+    
+    view bg k
+    view fill
+    view volu
+    view disp
+    view elem
+    
+    frame
+    zoom 0.8
+    rot y
+    rot r 35
+    rot u 35
+    
+    ds $ds e $element
+    hcpy png
+    sys mv hcpy_1.png pngs/$(printf "%04d\n" $frame).png
+    
+    exit
+END
+
+xvfb-run -a --server-args="-screen 0 1024x768x24" $dir/cgx-212/cgx_2.12/src/cgx -b tmp.fbd > /dev/null # 2>&1
+    
+((frame++))
+done
+
+convert -delay 15 -loop 0 pngs/*.png temp.gif
+
+#rm pngs -R
