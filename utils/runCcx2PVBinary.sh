0f85bb6d820e87fef1d35dc8cb8738b41d4355e5
mmehrabadi3 <marmar@gatech.edu>
2017-06-05 08:12:25 -0400

    Added a step to mainBOP workflow to run ccx

--- utils/runCcx2PVBinary.sh
+++ utils/runCcx2PVBinary.sh
@@ -1,6 +1,24 @@
 #!/bin/bash 
 CcxExec=$1 
-InpName=$2
+ccxInputFile=$2
+WORK_DIR=$(pwd)
+
+execDir=$(dirname "${ccxInputFile}")
+ccxfilename=$(basename "$ccxInputFile")
+ccxfileRootName="${ccxfilename%%.*}"
+echo $ccxInputFile
+echo $ccxfilename
+echo $ccxfileRootName
+
+# Copy matLibFile to execution directory if provided
+if [ "$#" -eq 3 ]; then
+	matLibFile=$3
+	cp $matLibFile $execDir/
+fi
+
 chmod +x  $CcxExec
-# EXPORT TO EXODUSII FOR PARAVIEW SIMULATION
-$CcxExec  $InpName -o exo
+
+# Export to EXODUSII for ParaView simulation
+cd $execDir
+$WORK_DIR/$CcxExec  $ccxfileRootName  -o exo
+
