8852a97a44dd47b7713b3fe6f5b80e8a6e3c285f
marmar-m <marmar@parallelworks.com>
2017-07-24 15:30:43 -0400

    Add runSim.sh which is needed for running mainBOP_sep.sh

---
+++ utils/runSim.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+CcxExec=$1 
+ccxInputFile=$2
+
+if [ "$#" -eq 3 ]; then
+	matLibFile=$3
+fi
+
+
+WORK_DIR=$(pwd)
+
+pvpythonExtractScript=utils/extract_Box_json.py
+pythonPlotScript=utils/plot.py
+
+execDir=$(dirname "${ccxInputFile}")
+ccxfilename=$(basename "$ccxInputFile")
+ccxfileRootName="${ccxfilename%%.*}" 
+
+
+# Copy matLibFile to execution directory if provided
+
+if [ "$#" -eq 3 ]; then
+	cp $matLibFile $execDir/
+fi
+
+chmod +x  $CcxExec
+
+
+# Run simulation with EXODUSII output for ParaView 
+
+cd $execDir
+$WORK_DIR/$CcxExec  $ccxfileRootName  -o exo
+cd $WORK_DIR
+
+
